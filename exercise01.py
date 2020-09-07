18362d1cc2cd5f2455bcb69c1fc3f49ce361d308
Naickaco <cianocak2001@gmail.com>
2020-09-07 01:34:50 +0100

    Adding comments to the exercise.

--- exercise01.py
+++ exercise01.py
@@ -7,46 +7,87 @@ import os.path
 
 class Led:
 	def __init__(self, pin : int):
+		"""
+			Erstellt eine Led und registiert den pin als Ausgang. 
+			Jede Led beinhaltet einen Status. Der Status gibt an ob die Led angeschaltet oder ausgeschaltet ist.
+
+			Args:
+				pin (int): An dem angegeben Pin wird die Led angeschlossen. 
+		"""		
 		self.__pin = pin
 		self.__state = False
 		self.__Setup()
 
 	def __Setup(self):
+		# Registiert den Pin von Objekt als Ausgang, damit die Led ein- und ausgeschaltet werden kann. 
 		GPIO.setup(self.__pin, GPIO.OUT)
 
 	def __Update(self):
+		# Übermittelt den aktuellen Led Zustand zum Raspberry Pi.
 		GPIO.output(self.__pin, self.__state)
 	
 	def show(self):
+		# Schaltet den Zustand an und übergibt den Wert zur gleichen Zeit zum Raspberry Pi. 
 		self.__state = True
 		self.__Update()
 	
 	def hide(self):
+		# Schaltet den Zustand aus und übergibt den Wert zur gleichen Zeit zum Raspberry Pi. 
 		self.__state = False
 		self.__Update()
 
 	def toggle(self):
+		# Negiert den Zustand und übergibt den Wert zur gleichen Zeit zum Raspberry Pi. 
 		self.__state = not self.__state
 		self.__Update()
 
 	def get_state(self) -> bool:
+		""" 
+			Liefert den aktuellen Zustand der Led zurück. 
+
+			Returns:
+				bool: Greift auf state zu und liefert ihn zurück.  
+		"""
 		return self.__state
 
 class Taster:
 	def __init__(self, pin : int):
+		""" 
+			Erstellt eine Taster Instance und registiert den Taster als Eingabe. 
+
+			Args:
+				pin (int): Auf diesen Pin wird der Taster registiert. 
+		"""
+
 		self.__pin = pin
 		self.__Setup()
 	
 	def __Setup(self):
+		# Registiert den Taster als Eingang. 
 		GPIO.setup(self.__pin, GPIO.IN)
 	
-	def is_pressed(self):
-		return GPIO.input(self.__pin)
+	def is_pressed(self) -> bool:
+		"""
+			Überprüft ob ein Taster gedrückt wurde. 
+	
+			Returns:
+				bool: returns if the taster is pressed.
+
+			
+		
+		"""
+		return not GPIO.input(self.__pin) # Bei meinen Button sind die Zustände verkehrt rum, darum das not.
 
 class LoopHandler:
 	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 	def __init__(self):
+		""" 
+			Setzt den Modus von der Pin-Nummierung auf GPIO.BOARD.
+			Erstellt jeweils eine Led, eine Taster instance und zwei Hilfsvariablen. Eine Hilfsvariable erfasst die Zeit wie lange der Taster gedrückt wurde.
+			Die andere Hilfsvariable zeigt an ob der Taster schon mal gedrückt wurde oder losgelassen wurde (nur einmalig).
+			Wenn keine Datebank vorhanden ist, wird eine Datenbank erstellt.
+		"""
 		GPIO.setmode(GPIO.BOARD)
 		self.__taster = Taster(11)
 		self.__led = Led(7)
@@ -63,6 +104,12 @@ class LoopHandler:
 		self.__database.commit()
 
 	def onSave(self, state : bool):
+		""" 
+			Speichert den Stand der Leuchtdiode in der Datenbank. 
+
+			Args:
+				state (bool): Der Status der Leuchtdiode. 
+		"""		
 		c = self.__database.cursor()
 		if state:
 			s = "AN"
@@ -74,6 +121,9 @@ class LoopHandler:
 		self.__database.commit()
 	
 	def Loop(self):
+		"""
+			Führt die Schleife solange aus bis, sie Unterbrochen wird. 
+		"""
 		try:
 			while True:
 				self.Update()
@@ -82,8 +132,20 @@ class LoopHandler:
 			GPIO.cleanup()
 	
 	def Update(self):
-		s = not self.__taster.is_pressed()
-  
+		"""
+			Überprüft durchgehend ob der Taster gedrückt wurde, wenn ja 
+			wird die aktuellen Zeit in einer Hilfsvariable gespeichert und auf ein Signal gewartet bis losgelassen wird.
+
+			Sobald losgelassen wurde, wird wieder die aktuellen
+			Zeit mit der davorherigen gedrückten Zeit subtrahiert um die Zeit zu errechnen,
+			wie lange die der Taster gedrückt wurde.
+
+			Wenn der Taster länger als 500 Millisekunden gedrückt wurde, wird der Zustand der Led negiert und in einer Datenbank-Tabelle festgehalten. 
+		
+			Am Ende werden alle Werte wieder zurückgesetzt.
+		"""
+		s = self.__taster.is_pressed()
+
 		print("The taster state %d. self.__startTime -> %d self.__currentState : %d" % (s, self.__startTime, self.__currentState))
 		if s:  
 			if not self.__currentState:
